d75963bfeff5bd11ccadc666edbad1a0b3e2d695
Christoffer Lette <christoffer.lette@gmail.com>
2017-09-01 21:54:54 +0200

    Refactored lexer

--- Lette.DustSucker2000.Library/Lexer.fs
+++ Lette.DustSucker2000.Library/Lexer.fs
@@ -1,14 +1,5 @@
 ﻿namespace Lette.DustSucker2000.Library
 
-type Position = { Row : int; Column : int }
-
-[<AutoOpen>]
-module Position =
-    let startPosition = { Row = 1; Column = 1 }
-    let nextColumn position = { position with Column = position.Column + 1 }
-    let nextRow position = { position with Row = position.Row + 1; Column = 1 }
-
-
 type Token =
     | Number of int
     | Letter of char
@@ -21,7 +12,7 @@ module Lexer =
 
     let private knownLetters = "AELNRSW" :> char seq
 
-    let private (|IsNumber|_|) (c : char) =
+    let private (|IsDigit|_|) (c : char) =
         if '0' <= c && c <= '9' then
             int c - int '0' |> Some
         else
@@ -35,20 +26,25 @@ module Lexer =
 
     let tokenize (input : char seq) =
 
-        let rec parseChars chars currentPosition tokens =
-            match chars with
-            | [] -> tokens
-            | (c :: cs) ->
-                match c with
-                | IsNumber n      ->
-                    match tokens with
-                    | (Number m :: ts) -> parseChars cs (currentPosition |> nextColumn) (Number (m * 10 + n) :: ts)
-                    | _                -> parseChars cs (currentPosition |> nextColumn) (Number n            :: tokens)
-                | IsKnownLetter l -> parseChars cs (currentPosition |> nextColumn) (Letter l                     :: tokens)
-                | c when c = ' '  -> parseChars cs (currentPosition |> nextColumn) (Space                        :: tokens)
-                | c when c = '\r' -> parseChars cs (currentPosition |> nextColumn) (CarriageReturn               :: tokens)
-                | c when c = '\n' -> parseChars cs (currentPosition |> nextRow)    (LineFeed                     :: tokens)
-                | c               -> parseChars cs (currentPosition |> nextColumn) (Unknown (c, currentPosition) :: tokens)
+        let rec parse chars position tokens =
 
-        parseChars (input |> Seq.toList) startPosition []
+            let addDigit n cs =
+                match tokens with
+                | (Number m :: ts) -> parse cs (position |> nextColumn) (Number (m * 10 + n) :: ts)
+                | ts               -> parse cs (position |> nextColumn) (Number n            :: ts)
+
+            let addToken c cs =
+                match c with
+                | IsDigit n       -> addDigit n cs
+                | IsKnownLetter l -> parse cs (position |> nextColumn) (Letter l              :: tokens)
+                | c when c = ' '  -> parse cs (position |> nextColumn) (Space                 :: tokens)
+                | c when c = '\r' -> parse cs (position |> nextColumn) (CarriageReturn        :: tokens)
+                | c when c = '\n' -> parse cs (position |> nextRow)    (LineFeed              :: tokens)
+                | c               -> parse cs (position |> nextColumn) (Unknown (c, position) :: tokens)
+
+            match chars with
+            | []        -> tokens
+            | (c :: cs) -> addToken c cs
+
+        parse (input |> Seq.toList) startPosition []
             |> List.rev
---
+++ Lette.DustSucker2000.Library/Position.fs
@@ -0,0 +1,9 @@
+﻿namespace Lette.DustSucker2000.Library
+
+type Position = { Row : int; Column : int }
+
+[<AutoOpen>]
+module Position =
+    let startPosition = { Row = 1; Column = 1 }
+    let nextColumn position = { position with Column = position.Column + 1 }
+    let nextRow position = { position with Row = position.Row + 1; Column = 1 }
