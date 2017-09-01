c1db01347d089cb0464ebfd22a5f5bd0975eb310
Christoffer Lette <christoffer.lette@gmail.com>
2017-09-01 23:59:08 +0200

    Refactored entry point

--- Lette.DustSucker2000.Console/Program.fs
+++ Lette.DustSucker2000.Console/Program.fs
@@ -1,26 +1,36 @@
 ﻿open Lette.DustSucker2000.Library
 
+let printInput input =
+    printfn "Input:"
+    printfn "--------------------------------------"
+    printfn "%s" input
+    printfn "--------------------------------------"
+    input
+
+let printTokens tokens =
+    printfn "Found tokens:"
+    printfn "%A" tokens
+    tokens
+
+let printInstructions instructions =
+    printfn "Generated instructions:"
+    instructions |> List.iter (printfn "%A")
+
+let printParseResult instructions =
+    match instructions with
+    | Success is  -> printInstructions is
+    | Failure msg -> printfn "%s" msg
+    instructions
+
 [<EntryPoint>]
 let main argv =
 
-    let input = "6 7
-N 3 2
-RARAARARA
-"
-
-    printfn "Input:
---------------------------------------
-%s
---------------------------------------" input
-
-    let tokens = input |> Lexer.tokenize
-
-    tokens |> printfn "Found tokens:
-%A"
-
-    let instructions = tokens |> Parser.parse
-
-    instructions |> printfn "Generated instructions:
-%A"
+    "6 7\r\nN 3 2\r\nRARAARARA\r\n"
+        |> printInput
+        |> Lexer.tokenize
+        |> printTokens
+        |> Parser.parse
+        |> printParseResult
+        |> ignore
 
     0
